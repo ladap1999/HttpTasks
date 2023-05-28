52f34398c237428e87e8355e7f6d6e9f39c45762
ladap1999 <ladap1999>
2023-05-28 21:54:09 +0300

    add observers task

---
+++ UserService/clients/UserServiceClient.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using UserService.Extensions;
+using UserService.Utils;
+
+namespace UserService.Clients;
+
+public class UserServiceClient : BaseClient, IObservable<string>
+{
+    private readonly HttpClient _client = new HttpClient();
+    private static  readonly Lazy<UserServiceClient> _instance = new Lazy<UserServiceClient>(() => new UserServiceClient());
+    
+    public static UserServiceClient Instance => _instance.Value;
+    public async Task<HttpResponseMessage> RegisterUser(object request)
+    {
+        var httpRequestMessage = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri(UserServiceEndpoints.register_user),
+            Content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json")
+        };
+
+        HttpResponseMessage response = await _client.SendAsync(httpRequestMessage);
+        if (response.IsSuccessStatusCode)
+        {
+            foreach (var observer in _observers)
+            {
+                if (observer.Value.GetType().Name == "DeleteTestObserver")
+                {
+                    Detach(observer.Value);
+                    NotifyAllObservers(response.GetStringValue());
+                    Subscribe(observer.Value);
+                }
+                
+                if (observer.Value.GetType().Name == "TransactionTestObserver")
+                {
+                    Detach(observer.Value);
+                    NotifyAllObservers(response.GetStringValue());
+                    Subscribe(observer.Value);
+                }
+            }
+        }
+
+        return response;
+    }
+
+    public async Task<HttpStatusCode> DeleteUser(int id)
+    {
+        var deleteUserRequest = new HttpRequestMessage
+        {
+            Method = HttpMethod.Delete,
+            RequestUri = new Uri(($"{UserServiceEndpoints.delete_user}?userId={id}"))
+        };
+
+        HttpResponseMessage response = await _client.SendAsync(deleteUserRequest);
+        if (response.IsSuccessStatusCode)
+        {
+            foreach (var observer in _observers)
+            {
+                if (observer.Value.GetType().Name == "TestDataObserver")
+                {
+                    Detach(observer.Value);
+                    NotifyAllObservers(Convert.ToString(id));
+                    Subscribe(observer.Value);
+                }
+                
+                if (observer.Value.GetType().Name == "TransactionTestObserver")
+                {
+                    Detach(observer.Value);
+                    NotifyAllObservers(response.GetStringValue());
+                    Subscribe(observer.Value);
+                }
+            }
+        }
+        return response.StatusCode;
+    }
+
+    public async Task<HttpResponseMessage> GetUserStatus(int id)
+    {
+        var getUserStatusRequest = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"{UserServiceEndpoints.get_user_status}?userId={id}")
+        };
+
+        HttpResponseMessage response = await _client.SendAsync(getUserStatusRequest);
+        return response;
+    }
+
+    public async Task<HttpStatusCode> SetUserStatus(int id, bool isActive)
+    {
+        var setUserStatusRequest = new HttpRequestMessage
+        {
+            Method = HttpMethod.Put,
+            RequestUri = new Uri($"{UserServiceEndpoints.set_user_status}?userId={id}&newStatus={isActive}")
+        };
+
+        HttpResponseMessage response = await _client.SendAsync(setUserStatusRequest);
+        return response.StatusCode;
+    }
+
+    public IDisposable Subscribe(IObserver<string> observer)
+    {
+       _observers.TryAdd(observer.GetType().Name, observer);
+       
+       return null;
+    }
+    
+    public void Detach(IObserver<string> observer)
+    {
+       _observers.Remove(observer.GetType().Name, out observer);
+    }
+    
+    public void NotifyAllObservers(string id)
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Value.OnNext(id);
+        }
+    }
+}
---
+++ UserService/observers/TestDataObserver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace UserService.observers;
+
+public class TestDataObserver : BaseObserver, IObserver<string>
+{
+    public void OnCompleted()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnError(Exception error)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnNext(string id)
+    {
+        _listOfIds.TryAdd(id, id);
+    }
+
+    public IDictionary<string, string> GetAllIds()
+    {
+        return _listOfIds;
+    }
+}
